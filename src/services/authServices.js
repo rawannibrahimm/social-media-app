ae170999d32a8afa7c5aef52f6e16f5c8dff0e5b
Rawan Ibrahim <rawannibrahimm288@gmail.com>
2026-02-04 08:37:50 +0200

    fifth_commit

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -3,11 +3,11 @@ import api from "../api/api";
 // we mean this function will be sent formData to it
 // using axios makes us can send the dataForm afterwards the API directly without the nead to add headers object
 export async function signUpUser(formData) {
-    const response = await api.post("/users/signup", formData);
-    return response.data;
+    const data = await api.post("/users/signup", formData);
+    return data;
 }
 
 export async function signInUser(formData) {
-    const response = await api.post("/users/signin", formData);
-    return response.data;
+    const data = await api.post("/users/signin", formData);
+    return data;
 }
